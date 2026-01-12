8324719e600b2806fc6377e4fb5679f55e623ec3
nkh97430@gmail.com <nkh97430@gmail.com>
2026-01-12 11:05:52 +0100

    update Footer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./Pages/Home.tsx";
 import Jadopte from "./Pages/Jadopte.tsx";
+import Footer from "./components/Footer.tsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/Jadopte",
     element: <Jadopte/>
+  },
+  {
+    path: "/Footer",
+    element: <Footer/>
   }
 ])
 const queryClient = new QueryClient();
